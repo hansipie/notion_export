138ce702745bf51b06499f728c7fbf3fecc47964
Hansi P <hansipie@users.noreply.github.com>
2022-02-21 08:28:40 +0100

    first working

--- export.py
+++ export.py
@@ -1,30 +1,97 @@
+import csv
+import os
+import shutil
+import tempfile
+import zipfile
 import requests
+import json
 import yaml
 
-class MyExport:
-
-    def __init__(self):
-        """
-        Gets required variable data from config yaml file.
-        """
-        with open("my_variable.yml", 'r') as stream:
-            try:
-                self.my_variables_map = yaml.safe_load(stream)
-            except yaml.YAMLError as exc:
-                print("[Error]: while reading yml file", exc)
-            self.my_variables_map["NOTION_ENTRIES"] = {}
-            self.getTaskId()
-
-    def getTaskId(self):
-        url = "https://www.notion.so/api/v3/enqueueTask"
-        headers = {
-            'Cookie': 'token2=' + self.my_variables_map["MY_NOTION_INTERNAL_TOKEN" + '; ']
+def EnqueueTask(id, token):
+    url = "https://www.notion.so/api/v3/enqueueTask"
+    payload = json.dumps({
+        "task": {
+            "eventName": "exportBlock",
+            "request": {
+            "block": {
+                "id": id
+            },
+            "recursive": False,
+            "exportOptions": {
+                "exportType": "markdown",
+                "timeZone": "Europe/Paris",
+                "locale": "en"
+            }
+            }
         }
-        body = {}
-        response = requests.post(url, headers=headers, json=body)
-        self.my_variables_map["TASK_ID"] = response.json()["taskId"]                
+    })
+    headers = {
+    'Cookie': 'token_v2=' +  token,
+    'Content-Type': 'application/json'
+    }
+    response = requests.request("POST", url, headers=headers, data=payload)
+    resp = response.json()
+    return(resp["taskId"])
 
+def GetTasks(taskid, token):
+    url = "https://www.notion.so/api/v3/getTasks"
+    payload = json.dumps({
+    "taskIds": [
+        taskid
+    ]
+    })
+    headers = {
+    'Cookie': 'token_v2=' + token,
+    'Content-Type': 'application/json'
+    }
 
+    response = requests.request("POST", url, headers=headers, data=payload)
+    
+    return(response.json())
 
-if __name__ == "__main__":
-    MyExport().UpdateIndefinitely()
+def DownloadArchive(url, dest):
+    print("Download url: " + url)
+    payload={}
+    headers = {}
+    response = requests.request("GET", url, headers=headers, data=payload)
+    destfile = os.path.join(dest,"archive.zip")
+    with open(destfile, "wb") as f:
+        f.write(response.content)
+    return(destfile)
+
+with open("my_variables.yml", 'r') as stream:
+    try:
+        my_variables_map = yaml.safe_load(stream)
+    except yaml.YAMLError as exc:
+        print("[Error]: while reading yml file", exc)
+
+taskid=EnqueueTask(my_variables_map["DATABASE_ID"], my_variables_map["MY_NOTION_INTERNAL_TOKEN"])
+
+dirpath = tempfile.mkdtemp()
+filelist = []
+loop = 1
+while (loop == 1):
+    resp = GetTasks(taskid, my_variables_map["MY_NOTION_INTERNAL_TOKEN"])
+    for v in resp["results"]:
+        print("state: " + v["state"])
+        if (v["state"] == "success"):
+            if (v["status"]["type"] == "complete"):
+                print("status: " + v["status"]["type"])
+                archive = DownloadArchive(v["status"]["exportURL"], dirpath)
+                with zipfile.ZipFile(archive, 'r') as zip_ref:
+                    filelist += zip_ref.namelist()
+                    zip_ref.extractall(dirpath)
+                loop = 0
+                break
+        print("retry")
+
+for f in filelist:
+    myfile=os.path.join(dirpath, f)
+    with open(myfile, newline='') as csvfile:
+        spamreader = csv.reader(csvfile)
+        for row in spamreader:
+            print(row)
+
+shutil.rmtree(dirpath)
+print("Done")
+
