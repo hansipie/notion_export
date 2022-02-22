fdd5668e375a3e280579a45d332476f224c6a94c
Hansi P <hansipie@users.noreply.github.com>
2022-02-22 22:26:45 +0100

    refactoring
    turn it into a class

--- export.py
+++ export.py
@@ -8,84 +8,88 @@ import requests
 import json
 import yaml
 
-def EnqueueTask(id, token):
-    url = "https://www.notion.so/api/v3/enqueueTask"
-    payload = json.dumps({
-        "task": {
-            "eventName": "exportBlock",
-            "request": {
-            "block": {
-                "id": id
-            },
-            "recursive": False,
-            "exportOptions": {
-                "exportType": "markdown",
-                "timeZone": "Europe/Paris",
-                "locale": "en"
+class NotionExport:
+
+    def __init__(self):
+        with open("my_variables_priv.yml", 'r') as stream:
+            try:
+                self.my_variables_map = yaml.safe_load(stream)
+            except yaml.YAMLError as exc:
+                print("[Error]: while reading yml file", exc)
+        self.my_variables_map["TASK_ID"]=self.EnqueueTask()
+
+    def EnqueueTask(self):
+        url = "https://www.notion.so/api/v3/enqueueTask"
+        payload = json.dumps({
+            "task": {
+                "eventName": "exportBlock",
+                "request": {
+                "block": {
+                    "id": self.my_variables_map["DATABASE_ID"]
+                },
+                "recursive": False,
+                "exportOptions": {
+                    "exportType": "markdown",
+                    "timeZone": "Europe/Paris",
+                    "locale": "en"
+                }
+                }
             }
-            }
+        })
+        headers = {
+        'Cookie': 'token_v2=' + self.my_variables_map["MY_NOTION_INTERNAL_TOKEN"],
+        'Content-Type': 'application/json'
         }
-    })
-    headers = {
-    'Cookie': 'token_v2=' +  token,
-    'Content-Type': 'application/json'
-    }
-    response = requests.request("POST", url, headers=headers, data=payload)
-    resp = response.json()
-    return(resp["taskId"])
+        response = requests.request("POST", url, headers=headers, data=payload)
+        resp = response.json()
+        return(resp["taskId"])
 
-def GetTasks(taskid, token):
-    url = "https://www.notion.so/api/v3/getTasks"
-    payload = json.dumps({
-    "taskIds": [
-        taskid
-    ]
-    })
-    headers = {
-    'Cookie': 'token_v2=' + token,
-    'Content-Type': 'application/json'
-    }
+    def GetTasks(self):
+        url = "https://www.notion.so/api/v3/getTasks"
+        payload = json.dumps({
+        "taskIds": [
+            self.my_variables_map["TASK_ID"]
+        ]
+        })
+        headers = {
+        'Cookie': 'token_v2=' + self.my_variables_map["MY_NOTION_INTERNAL_TOKEN"],
+        'Content-Type': 'application/json'
+        }
 
-    response = requests.request("POST", url, headers=headers, data=payload)
-    
-    return(response.json())
+        response = requests.request("POST", url, headers=headers, data=payload)
+        
+        return(response.json())
 
-def DownloadArchive(url, dest):
-    print("Download url: " + url)
-    payload={}
-    headers = {}
-    response = requests.request("GET", url, headers=headers, data=payload)
-    destfile = os.path.join(dest,"archive.zip")
-    with open(destfile, "wb") as f:
-        f.write(response.content)
-    return(destfile)
+    def DownloadArchive(self, url, dest):
+        print("Download url: " + url)
+        payload={}
+        headers = {}
+        response = requests.request("GET", url, headers=headers, data=payload)
+        destfile = os.path.join(dest,"archive.zip")
+        with open(destfile, "wb") as f:
+            f.write(response.content)
+        return(destfile)
 
-with open("my_variables.yml", 'r') as stream:
-    try:
-        my_variables_map = yaml.safe_load(stream)
-    except yaml.YAMLError as exc:
-        print("[Error]: while reading yml file", exc)
+    def GetCSVfile(self):
+        mytime = str(math.floor(time.time()))
+        destpath = os.path.join("files", mytime)
+        temppath = tempfile.mkdtemp()
+        loop = 1
+        while (loop == 1):
+            resp = self.GetTasks()
+            for v in resp["results"]:
+                print("state: " + v["state"])
+                if (v["state"] == "success"):
+                    if (v["status"]["type"] == "complete"):
+                        print("status: " + v["status"]["type"])
+                        archive = self.DownloadArchive(v["status"]["exportURL"], temppath)
+                        with zipfile.ZipFile(archive, 'r') as zip_ref:
+                            zip_ref.extractall(destpath)
+                        loop = 0
+                        break
+                print("retry")
+        shutil.rmtree(temppath)        
 
-taskid=EnqueueTask(my_variables_map["DATABASE_ID"], my_variables_map["MY_NOTION_INTERNAL_TOKEN"])
-
-mytime = str(math.floor(time.time()))
-destpath = os.path.join("files", mytime)
-temppath = tempfile.mkdtemp()
-loop = 1
-while (loop == 1):
-    resp = GetTasks(taskid, my_variables_map["MY_NOTION_INTERNAL_TOKEN"])
-    for v in resp["results"]:
-        print("state: " + v["state"])
-        if (v["state"] == "success"):
-            if (v["status"]["type"] == "complete"):
-                print("status: " + v["status"]["type"])
-                archive = DownloadArchive(v["status"]["exportURL"], temppath)
-                with zipfile.ZipFile(archive, 'r') as zip_ref:
-                    zip_ref.extractall(destpath)
-                loop = 0
-                break
-        print("retry")
-
-shutil.rmtree(temppath)
-print("Done")
-
+if __name__ == "__main__":
+    NotionExport().GetCSVfile()
+    print("Done.")
